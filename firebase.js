6454a9c8f6ef91509591205fe541df72a004f87a
sandevadiga <santhoshrdevadiga@gmail.com>
2024-05-03 22:58:01 +0530

    added all

--- firebase.js
+++ firebase.js
@@ -3,17 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// require('dotenv').config({ path: require('find-config')('.env') })
- 
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,4 +19,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { app, auth, db, storage };
+export { app, auth, db ,storage};
--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-import webpack from 'webpack';
-
-
-import { config as dotenvConfig } from 'dotenv';
-// import { EnvironmentPlugin } from 'webpack';
-const { EnvironmentPlugin } = webpack;
-
-const { parsed: localEnv } = dotenvConfig();
-
-const nextConfig = {
-  webpack(config) {
-    config.plugins.push(new EnvironmentPlugin(localEnv));
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
+
+// import webpack from 'webpack';
+
+
+// import { config as dotenvConfig } from 'dotenv';
+// // import { EnvironmentPlugin } from 'webpack';
+// const { EnvironmentPlugin } = webpack;
+
+// const { parsed: localEnv } = dotenvConfig();
+
+// const nextConfig = {
+//   webpack(config) {
+//     config.plugins.push(new EnvironmentPlugin(localEnv));
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
